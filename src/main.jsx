e37fbcc428ec5c343b0249b5acb37da714520094
Robin <robin.sliet@gmail.com>
2024-07-08 11:16:06 +0530

    Refactor: Improve structure and organize global theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider, createTheme } from "@mui/material";
-
-const mainTheme = createTheme({
-  palette: {
-    primary: {
-      main: '#6E07F3',
-    },
-    secondary: {
-      main: '#5BE9B9',
-    },
-    transparent: {
-      main: 'rgba(255, 255, 255, 0.12)',
-    },
-    fBlack:{
-      main: "#28282B",
-    }
-  },
-});
+import theme from "./styles/theme.js";
+import { ThemeProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6E07F3",
+    },
+    secondary: {
+      main: "#5BE9B9",
+    },
+    transparent: {
+      main: "rgba(255, 255, 255, 0.12)",
+    },
+    fBlack: {
+      main: "#28282B",
+    },
+  },
+});
+
+export default theme;
